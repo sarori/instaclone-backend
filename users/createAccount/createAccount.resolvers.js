6f4bd3045a27061611a259815b326322466fd57a
sarori <salo3678@gmail.com>
2021-04-10 01:25:56 +0200

    Divide and Conquer

--- users/createAccount/createAccount.resolvers.js
+++ users/createAccount/createAccount.resolvers.js
@@ -1,7 +1,5 @@
-import client from "../client"
-import jwt from "jsonwebtoken"
+import client from "../../client"
 import bcrypt from "bcrypt"
-require("dotenv").config()
 
 export default {
 	Mutation: {
@@ -37,30 +35,6 @@ export default {
 				})
 			} catch (e) {
 				return e
-			}
-		},
-		login: async (_, { username, password }) => {
-			//find user with args.username
-			const user = await client.user.findFirst({ where: { username } })
-			if (!user) {
-				return {
-					ok: false,
-					error: "User not found",
-				}
-			}
-			//check password with args.password
-			const passwordOk = await bcrypt.compare(password, user.password)
-			if (!passwordOk) {
-				return {
-					ok: false,
-					error: "Incorrect password.",
-				}
-			}
-			//Everything is fine,issue a token and send it to the user
-			const token = await jwt.sign({ id: user.id }, process.env.SECRET_KEY)
-			return {
-				ok: true,
-				token,
 			}
 		},
 	},

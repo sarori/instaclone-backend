b021e6d709eebd2099887fd40691df29df8ca011
sarori <salo3678@gmail.com>
2021-04-09 22:59:50 +0200

    seeProfile

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -4,32 +4,38 @@ import bcrypt from "bcrypt"
 export default {
 	Mutation: {
 		createAccount: async (_, { firstName, lastName, username, email, password }) => {
-			// check if username or email are already on DB.
-			const existingUser = await client.user.findFirst({
-				where: {
-					OR: [
-						{
-							username,
-						},
-						{
-							email,
-						},
-					],
-				},
-			})
-			// Hash password
-			// save and return the user
-
-			const uglyPassword = await bcrypt.hash(password, 10)
-			return client.user.create({
-				data: {
-					username,
-					email,
-					firstName,
-					lastName,
-					password: uglyPassword,
-				},
-			})
+			try {
+				// check if username or email are already on DB.
+				const existingUser = await client.user.findFirst({
+					where: {
+						OR: [
+							{
+								username,
+							},
+							{
+								email,
+							},
+						],
+					},
+				})
+				if (existingUser) {
+					throw new Error("This username or password is already taken")
+				}
+				// Hash password
+				// save and return the user
+				const uglyPassword = await bcrypt.hash(password, 10)
+				return client.user.create({
+					data: {
+						username,
+						email,
+						firstName,
+						lastName,
+						password: uglyPassword,
+					},
+				})
+			} catch (e) {
+				return e
+			}
 		},
 	},
 }

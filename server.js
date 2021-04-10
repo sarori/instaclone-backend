67561ebae9ffd23dceb851147a4662e2fdecdc57
sarori <salo3678@gmail.com>
2021-04-10 14:50:13 +0200

    Authentication part two

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ import schema from "./schema"
 
 const server = new ApolloServer({
 	schema,
+	context: {
+		token:
+			"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NiwiaWF0IjoxNjE4MDQyNTQwfQ.CMCuvBjCsz2KmPGhjwQbcsN9b3ce40Vcn-TVfHzHw3g",
+	},
 })
 
 const PORT = process.env.PORT

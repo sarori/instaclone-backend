cf5fe46d2d1b22088fdf3d4040c468b6352e04fc
sarori <salo3678@gmail.com>
2021-04-15 17:06:39 +0200

    Computed Fields part two

--- src/users/users.resolvers.js
+++ src/users/users.resolvers.js
@@ -1,11 +1,34 @@
 //Resolver of Field in Graphql type User
 
+import client from "../client"
+
 export default {
 	User: {
-		totalFollowing: (root) => {
-			console.log(root)
-			return 0
+		totalFollowing: ({ id }) =>
+			client.user.count({
+				where: {
+					followers: {
+						some: {
+							id,
+						},
+					},
+				},
+			}),
+		totalFollowers: ({ id }) =>
+			client.user.count({
+				where: {
+					following: {
+						some: {
+							id,
+						},
+					},
+				},
+			}),
+		isMe: ({ id }, _, { loggedInUser }) => {
+			if (!loggedInUser) {
+				return false
+			}
+			return id === loggedInUser.id
 		},
-		totalFollowers: () => 999,
 	},
 }
--- src/users/users.typeDefs.js
+++ src/users/users.typeDefs.js
@@ -15,7 +15,7 @@ export default gql`
 		followers: [User]
 		totalFollowing: Int!
 		totalFollowers: Int!
+		isMe: Boolean!
 	}
 `
 // isFollowing: Boolean!
-// isMe: Boolean!

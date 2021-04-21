e807b7d80e78656190de83fcf137a33bcc7bbb9c
sarori <salo3678@gmail.com>
2021-04-21 20:39:37 +0200

    Upload Photo part Two

--- src/photos/uploadPhoto/uploadPhoto.resolvers.js
+++ src/photos/uploadPhoto/uploadPhoto.resolvers.js
@@ -1,12 +1,34 @@
+import client from "../../client"
+
 export default {
 	Mutation: {
 		uploadPhoto: async (_, { file, caption }, { loggedInUser, protectedResolver }) => {
 			if (caption) {
 				//parse caption
+				const hashtags = caption.match(/#[\w]+/g)
+				console.log(hashtags)
 				//get or create Hashtags
 			}
 			//save the photo wtih the Parsed hashtags
 			//add the photo to the hashtags
+			client.photo.create({
+				data: {
+					file,
+					caption,
+					hashtags: {
+						connectOrCreate: [
+							{
+								where: {
+									hashtag: "",
+								},
+								create: {
+									hashtag: "",
+								},
+							},
+						],
+					},
+				},
+			})
 		},
 	},
 }

a3095ac36cb7af7791932d1f1bbde3532e31dcca
sarori <salo3678@gmail.com>
2021-04-27 12:28:33 +0200

    protectedResolver Refactor

--- src/users/user.utils.js
+++ src/users/user.utils.js
@@ -18,45 +18,19 @@ export const getUser = async (token) => {
 	}
 }
 
-// export const protectedResolver = (user) => {
-// 	if (!user) {
-// 		throw new Error("You need to login.")
-// 	}
-// }
-
 export function protectedResolver(ourResolver) {
 	return function (root, args, context, info) {
 		if (!context.loggedInUser) {
-			return {
-				ok: false,
-				error: "Please log in to perform this action.",
+			const query = info.operation.operation === "query"
+			if (query) {
+				return null
+			} else {
+				return {
+					ok: false,
+					error: "Please log in to perform this action.",
+				}
 			}
 		}
 		return ourResolver(root, args, context, info)
 	}
 }
-// export const protectedResolver = (user) => {
-// 	if (!user) {
-// 		throw new Error("You need to login.")
-// 	}
-// }
-
-// export const protectedResolver = (user) => {
-// 	if (!user) {
-// 		return {
-// 			ok: false,
-// 			error: "You need to login.",
-// 		}
-// 	}
-// }
-
-// currying
-// export const protectedResolver = (ourResolver) => (root, args, context, info) => {
-// 	if (!context.loggedInUser) {
-// 		return {
-// 			ok: false,
-// 			error: " Please log in to perform this action.",
-// 		}
-// 	}
-// 	return ourResolver(root, args, context, info)
-// }

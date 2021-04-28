6b6eca72562c8a01e5951e22d1c8419281484bfd
sarori <salo3678@gmail.com>
2021-04-28 16:31:49 +0200

    readMessage

---
+++ src/messages/readMessage/readMessage.resolvers.js
@@ -0,0 +1,42 @@
+import client from "../../client"
+
+export default {
+	Mutation: {
+		readMessage: async (_, { id }, { loggedInUser, protectedResolver }) => {
+			protectedResolver(loggedInUser)
+			const message = await client.message.findFirst({
+				where: {
+					id,
+					userId: {
+						not: loggedInUser.id,
+					},
+					room: {
+						users: {
+							some: {
+								id: loggedInUser.id,
+							},
+						},
+					},
+				},
+				select: {
+					id: true,
+				},
+			})
+			if (!message) {
+				return {
+					ok: false,
+					error: "Message not found",
+				}
+			}
+			await client.message.update({
+				where: {
+					id,
+				},
+				data: {
+					read: true,
+				},
+			})
+			return true
+		},
+	},
+}
